020fcb621a3ca8460935cd4524e0afb89d2151a4
Ayush Raj <9ayushraj@gmail.com>
2026-06-18 09:40:18 +0530

    day 2nd of commiting

---
+++ DSA/Fourth.java
@@ -0,0 +1,138 @@
+public class Fourth {
+    public static void main(String[] args) {
+        // for(int i=0; i<5; i++) {
+        //     for(int j=0; j<5; j++){
+        //         if(i == 0 || j == 2){
+        //             System.out.print("*");
+        //         } else {
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+        
+        // -------------------
+        
+        // for(int i=0; i<5; i++){
+        //     for(int j=0; j<5; j++){
+        //         if(j == 0|| j == 4|| i==2){
+        //             System.out.print("*");
+        //         } else {
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+        
+        // -------------------
+
+        // for(int i=0; i<5; i++){
+        //     for(int j=0; j<5; j++){
+        //         if(j == 0|| j == 4|| i==0 || i == 4){
+        //             System.out.print("*");
+        //         } else {
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+        // -----------------------
+
+        // for(int i=0; i<5; i++){
+        //     for(int j=0; j<5; j++){
+        //         if(j == 0 || i == 4){
+        //             System.out.print("*");
+        //         } else {
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+        // -------------------------
+
+        // for(int i=0; i<5; i++){
+        //     for(int j=0; j<5; j++){
+        //         if(i == j || (i+j) == 4){
+        //             System.out.print("*");
+        //         } else {
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+        
+        // --------------------------
+        
+        // for(int i=0; i<5; i++){
+        //     for(int j=0; j<5; j++){
+        //         if(i == 0 || i==4 || j == 2){
+        //             System.out.print("*");
+        //         } else {
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+        
+        // --------------------------
+        
+        // for(int i=0; i<5; i++){
+        //     for(int j=0; j<5; j++){
+        //         if(i == 0 || i==4 || j == 0){
+        //             System.out.print("*");
+        //         } else {
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+        
+        // --------------------------
+        
+        // for(int i=0; i<5; i++){
+        //     for(int j=0; j<5; j++){
+        //         if(i == 0 || i==2 || j == 0 || j == 4){
+        //             System.out.print("*");
+        //         } else {
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+        
+        // --------------------------
+        
+        // for(int i=1; i<=20; i=i+5){
+        //     for(int j=i; j<i+5; j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+
+        // --------------------------
+        
+        // for(int i=1; i<=5; i++){
+        //     for(int j=i; j<i+5; j++){
+        //         System.out.print(i);
+        //     }
+        //     System.out.println();
+        // }
+
+        // --------------------------
+        
+        int x = 1;
+        for(int i=0; i< 5; i++){
+            for(int j=0; j<5; j++){
+                System.out.print(x % 2 + " ");
+                x++;
+            }
+            System.out.println();
+        }
+
+        // -----------------------
+        
+        
+    }
+}
